920e4dc3ddf06312e85f3ef362e347a8e609cfd8
Krismigos <zeligans.1337@gmail.com>
2022-11-27 22:25:10 +0100

    Search style

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,6 +1,18 @@
+import { FormSearch, BtnSearch } from './style';
 
 export const Search = () => {
-    return (
-        'Search'
-    )
-}
+  return (
+    <div className="wrapper">
+      <FormSearch>
+        <form action="">
+          <input
+            type="text"
+            name="serach"
+            placeholder="Запишіть назву міста..."
+          />
+        </form>
+        <BtnSearch type="submit">Пошук</BtnSearch>
+      </FormSearch>
+    </div>
+  );
+};
---
+++ src/components/Search/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const FormSearch = styled.div`
+  margin-top: 50px;
+  display: flex;
+  margin-bottom: 50px;
+`;
+
+export const BtnSearch = styled.button`
+width: 100px;
+`
