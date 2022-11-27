291ff74f2d23f8d239fa842cdd6787e1d49383c8
Kindual <110603179+Kindual@users.noreply.github.com>
2022-11-28 00:39:04 +0200

    Update WeatherNow.jsx

--- src/components/WeatherNow/WeatherNow.jsx
+++ src/components/WeatherNow/WeatherNow.jsx
@@ -1,5 +1,21 @@
 export const WeatherNow = () => {
     return (
-        'WeatherNow'
+        <section className="section">
+            <div>
+                <h2 className="place">Siliguri</h2>
+                <p className="a-date">mon 3/5/21</p>
+            </div>
+            <input type="checkbox" className="like" />
+            <div className="weather-temp">
+                <p className="temperature">
+                    <span className="t-num">30</span><span className="t-icons">&deg;</span><span className="t-cell">&#99;</span>
+                </p>
+                <img src="" alt="" />
+            </div>
+            <ul className="list amplitude">
+                <li className="amplitude-item"><a href="" className="amplitude-link"> Highest<br/>34&deg;C</a></li>
+                <li className="amplitude-item"><a href="" className="amplitude-link">Lowest<br/>26&deg;C</a></li>
+            </ul>
+        </section>
     )
 }
