0e6aaf2ef383aef446632221b7c6e415f9fac23f
artipavl <40296816+artipavl@users.noreply.github.com>
2022-11-28 16:24:45 +0200

    add css

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,11 +1,11 @@
-import { WeatherContainer, SecondBox, Diagrams} from './style';
+import {Diagrams} from './style';
 
 export const WeatherForecast = () => {
   return (
-    <div className="wrapper">
-      <WeatherContainer>
-        <SecondBox><Diagrams><h2>5 Day Weather Forecast</h2></Diagrams></SecondBox>
-      </WeatherContainer>
+    <div className="container">
+      <Diagrams>
+        <h2>5 Day Weather Forecast</h2>
+      </Diagrams>
     </div>
   );
 };
